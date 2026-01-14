4523e5b3c96f67bc3707af3a2a7a25a55445eb2b
yeswanthganesh <yeswanthganeshk@gmail.com>
2026-01-14 18:53:19 +0530

    Add cart and wishlist count functionality

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+let cartCount = 0;
+let wishCount = 0;
+
 function addToCart() {
-  alert("Item added to cart!");
+  cartCount++;
+  document.getElementById("cart-count").innerText = cartCount;
+}
+
+function addToWishlist() {
+  wishCount++;
+  document.getElementById("wish-count").innerText = wishCount;
 }
